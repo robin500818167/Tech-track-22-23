9c59dd6110b015c317f187cb63ad28a270cc2385
Robin500818167 <robin.van.houten@hva.nl>
2022-10-25 21:45:49 +0200

    Update data.js

---
+++ data.js
@@ -0,0 +1,10 @@
+console.log("Het werkt!")
+// Replace ./data.json with your JSON feed
+fetch('./swagger.json').then(response => {
+    return response.json();
+  }).then(data => {
+    // Work with JSON data here
+    console.log(data);
+  }).catch(err => {
+    // Do something for an error here
+  });
