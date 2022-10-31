f385d1bb5d1004e488d09adffa4fd2a0f71a3c32
Robin500818167 <robin.van.houten@hva.nl>
2022-10-31 16:15:30 +0100

    working boilerplate

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,21 @@
 // Our bundler automatically creates styling when imported in the main JS file!
 import '../styles/style.css'
-
 // We can use node_modules directely in the browser!
 import * as d3 from 'd3';
+import CONFIG from './config.js';
+import request from './request.js';
+import makeHtml from './make.js';
 
-console.log('Hello, world!');
+const data = await request(CONFIG.url);
+
+let amount = 0;
+
+console.log(data);
+
+makeHtml(data
+	.map(item => item['Welke kleur kledingstukken heb je aan vandaag? (Meerdere antwoorden mogelijk natuurlijk...)'])
+	.map(item => item.split(',').map(color => color.trim())).flat()
+	// .filter(item => item == 'Zwart' || item === 'Wit') // Zwart en wit zijn awesome!;
+	.reduce((acc, curr) => {
+		return acc[curr] ? ++acc[curr] : acc[curr] = 1, acc
+	}, {}), document.querySelector('table'))
