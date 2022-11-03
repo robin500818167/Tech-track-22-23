18c16ca75fe6df481075b96a4e1c7fd7c631cfa5
Robin500818167 <robin.van.houten@hva.nl>
2022-11-03 14:49:59 +0100

    banner

--- scripts/config.js
+++ scripts/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-	url: 'https://opensheet.elk.sh/1tO9SS1f9RQZ0Ay0BmkczJVkCxWcAhzGtcSDpzheKTZI/responses'
+	url: 'scripts/swagger.json'
 }
 
 export default CONFIG;
--- scripts/main.js
+++ scripts/main.js
@@ -4,18 +4,10 @@ import '../styles/style.css'
 import * as d3 from 'd3';
 import CONFIG from './config.js';
 import request from './request.js';
-import makeHtml from './make.js';
 
 const data = await request(CONFIG.url);
 
 let amount = 0;
 
-console.log(data);
+console.log(data.paths);
 
-makeHtml(data
-	.map(item => item['Welke kleur kledingstukken heb je aan vandaag? (Meerdere antwoorden mogelijk natuurlijk...)'])
-	.map(item => item.split(',').map(color => color.trim())).flat()
-	// .filter(item => item == 'Zwart' || item === 'Wit') // Zwart en wit zijn awesome!;
-	.reduce((acc, curr) => {
-		return acc[curr] ? ++acc[curr] : acc[curr] = 1, acc
-	}, {}), document.querySelector('table'))
